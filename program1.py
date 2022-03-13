05882a42c419e529c9a57fdb34d8c00c736875a4
Supreeth.M <85946079+Supreeth2319@users.noreply.github.com>
2022-03-13 11:35:22 +0530

    Update program1.py

--- program1.py
+++ program1.py
@@ -1,21 +1,3 @@
-'''#1a to calculate sum of digits
-n=int(input("Enter a number:"))
-print("The number is",n)
-sum=0
-while(n>0 or sum>9):
-    if(n==0):
-        n=sum
-        sum=0
-    sum=sum+n%10
-    n=n//10
-print(f"sum of the {n} is {sum}")
-if(sum==1):
-    print("it is a magic number")
-else:
-    print("it is not a magic number")
-'''
-
-#1b printing all prime number less than particular number
 
 n=int(input("Enter a number:"))
 print("the prime numbers upto",n,"are")
